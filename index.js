b362974d7743419cc75d506c9cfbd67131f59e03
Miguel <workingbeta01@gmail.com>
2024-05-04 13:54:18 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require( 'express' );
 const cors = require('cors')
@@ -30,6 +31,12 @@ app.use( '/api/medicos',    require( './routes/medicos' ) );
 app.use( '/api/todo',       require( './routes/busquedas' ) );
 app.use( '/api/login',      require( './routes/auth' ) );
 app.use( '/api/upload',     require( './routes/uploads' ) );
+
+// Si no es ninguna de las rutas anteriores
+app.get( '*', ( req, res ) => {
+  res.sendFile( path.resolve( __dirname, 'public/index.html') );
+})
+
 
 
 
